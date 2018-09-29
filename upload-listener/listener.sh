2cae8df116e5589bea8ab46add2d0ae1af078f21
Wilfred Gee <wtylergee@gmail.com>
2018-09-29 20:25:58 +1000

    Clean

--- upload-listener/listener.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the Cloud SQL Proxy
-echo "Starting Cloud SQL Proxy"
-/app/cloud_sql_proxy -instances=panoptes-survey:us-central1:panoptes-meta=tcp:5432 &
-
-# Start the listener
-echo "Staring listener script"
-/opt/conda/bin/python3 /app/upload-listener/upload_listener.py
