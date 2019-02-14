387ec2c5e6644fabadba355436a02763a3d207f3
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-02-15 09:35:33 +1100

    Updating documentation and deploy files for CF

--- cf-storage-fits-uploaded/deploy.sh
+++ cf-storage-fits-uploaded/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 gcloud functions deploy \
-                 ack-fits-received \
-                 --entry-point ack_fits_received \
+                 ack-image-received \
+                 --entry-point ack_image_received \
                  --runtime python37 \
                  --trigger-resource panoptes-survey \
                  --trigger-event google.storage.object.finalize
