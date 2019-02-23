91772fadd472821f4a819f81cffbb485290bb72f
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-02-24 10:02:57 +1100

    Docker image based on `piaa`. Remove the `storage` module and just put the two helper functions in same file.

--- gce-plate-solver/app.py
+++ gce-plate-solver/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+from google.cloud import storage
 from google.cloud import bigquery
 
 import pandas as pd
@@ -12,7 +13,6 @@ from astropy.time import Time
 from pocs.utils.images import fits as fits_utils
 from piaa.utils.postgres import get_cursor
 from piaa.utils import pipeline
-import storage
 
 PROJECT_ID = os.getenv('PROJECT_ID', 'panoptes')
 BUCKET_NAME = os.getenv('BUCKET_NAME', 'panoptes-survey')
@@ -73,6 +73,47 @@ def solve_file():
 
     # Send the response
     return jsonify({'status': 'sources_extracted', 'file': fits_fn, }), 201
+
+
+def download_blob(source_blob_name, destination=None, bucket=None, bucket_name='panoptes-survey'):
+    """Downloads a blob from the bucket."""
+    if bucket is None:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+
+    blob = bucket.blob(source_blob_name)
+
+    # If no name then place in current directory
+    if destination is None:
+        destination = source_blob_name.replace('/', '_')
+
+    if os.path.isdir(destination):
+        destination = os.path.join(destination, source_blob_name.replace('/', '_'))
+
+    blob.download_to_filename(destination)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination))
+
+    return destination
+
+
+def upload_blob(source_file_name, destination, bucket=None, bucket_name='panoptes-survey'):
+    """Uploads a file to the bucket."""
+    if bucket is None:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+
+    # Create blob object
+    blob = bucket.blob(destination)
+
+    # Upload file to blob
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination))
 
 
 app.run(host='0.0.0.0', port=8080)
