42c6bdd62600310386cb753e1da27704862efe4b
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-02-25 06:39:35 +1100

    * Add Google cloud logging (not sure this is needed as we can just use `print`). * Add `echo` route for testing server up * DB cursor is global * Adding requirements

--- gce-plate-solver/app.py
+++ gce-plate-solver/app.py
@@ -5,6 +5,7 @@ from flask import jsonify
 
 from google.cloud import storage
 from google.cloud import bigquery
+import google.cloud.logging
 
 import pandas as pd
 from astropy import units as u
@@ -14,19 +15,37 @@ from pocs.utils.images import fits as fits_utils
 from piaa.utils.postgres import get_cursor
 from piaa.utils import pipeline
 
+# Instantiates a client
+logging_client = google.cloud.logging.Client()
+
+# Connects the logger to the root logging handler; by default this captures
+# all logs at INFO level and higher
+logging_client.setup_logging()
+
+import logging
+
 PROJECT_ID = os.getenv('PROJECT_ID', 'panoptes')
 BUCKET_NAME = os.getenv('BUCKET_NAME', 'panoptes-survey')
 bq_client = bigquery.Client()
 storage_client = storage.Client(project=PROJECT_ID)
 bucket = storage_client.get_bucket(BUCKET_NAME)
 
-db_cursor = get_cursor(port=5433, db_name='v702', db_user='panoptes')
-
 app = Flask(__name__)
+
+db_cursor = None
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    content = request.json
+    echo = content.get('echo', None)
+    return jsonify({'echo': echo}), 201
 
 
 @app.route('/solve', methods=['POST'])
 def solve_file():
+    global db_cursor
+
     content = request.json
     bucket_fits_fn = content.get('filename', None)
 
@@ -37,15 +56,21 @@ def solve_file():
     sequence_id = f'{unit_id}_{cam_id}_{seq_time}'
 
     # Download file blob from bucket
-    fz_fn = storage.download_blob(bucket_fits_fn, destination='/tmp', bucket=bucket)
+    logging.info(f'Downloading {bucket_fits_fn}')
+    fz_fn = download_blob(bucket_fits_fn, destination='/tmp', bucket=bucket)
 
     # Unpack the FITS file
     fits_fn = fits_utils.fpack(fz_fn, unpack=True)
 
     # Solve fits file
+    logging.info(f'Plate-solving {fits_fn}')
     solve_info = fits_utils.get_solve_field(fits_fn, timeout=90)
 
+    if db_cursor is None:
+        db_cursor = get_cursor(port=5433, db_name='v702', db_user='panoptes')
+
     # Lookup point sources
+    logging.info(f'Looking up sources for {fits_fn}')
     point_sources = pipeline.lookup_point_sources(
         fits_fn,
         force_new=True,
@@ -64,12 +89,13 @@ def solve_file():
     point_sources['sequence'] = sequence_id
 
     # Send to bigquery
+    logging.info(f'Sending {len(point_sources)} sources to bigquery')
     bq_client.load_table_from_dataframe(point_sources, sources_table_ref).result()
 
     # Upload solved file
     if solve_info is not None:
         fz_fn = fits_utils.fpack(fits_fn)
-        storage.upload_blob(fz_fn, bucket_fits_fn, bucket=bucket)
+        upload_blob(fz_fn, bucket_fits_fn, bucket=bucket)
 
     # Send the response
     return jsonify({'status': 'sources_extracted', 'file': fits_fn, }), 201
@@ -92,7 +118,7 @@ def download_blob(source_blob_name, destination=None, bucket=None, bucket_name='
 
     blob.download_to_filename(destination)
 
-    print('Blob {} downloaded to {}.'.format(
+    logging.info('Blob {} downloaded to {}.'.format(
         source_blob_name,
         destination))
 
@@ -111,7 +137,7 @@ def upload_blob(source_file_name, destination, bucket=None, bucket_name='panopte
     # Upload file to blob
     blob.upload_from_filename(source_file_name)
 
-    print('File {} uploaded to {}.'.format(
+    logging.info('File {} uploaded to {}.'.format(
         source_file_name,
         destination))
 
