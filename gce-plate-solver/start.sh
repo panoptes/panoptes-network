a3e953c302f08f91dc2dff5e480d8297cb8d7d40
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-02-26 09:29:21 +1100

    * Download data * Get pgpass from metadata server

--- gce-plate-solver/start.sh
+++ gce-plate-solver/start.sh
@@ -3,5 +3,11 @@
 echo "Starting Cloud SQL proxy"
 python3 /var/panoptes/PIAA/scripts/connect_cloud_sql_proxy.py --config /var/panoptes/PIAA/conf.yaml --verbose &
 
+echo "Getting astrometry.net files"
+python3 $POCS/pocs/utils/data.py
+
+echo "Getting DB passwords from metadata server"
+curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/pgpass" -H "Metadata-Flavor: Google" > $HOME/.pgpass
+
 echo "Starting PubSub listener"
 python3 pubsub-listener.py
