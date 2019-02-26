c58bf250da50eb678f7010b93db35852f0ad99e1
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-02-27 07:57:22 +1100

    Cleanup
    * More consistent variables names
    * Populate headers properly before sending to add-header-to-db CF.

--- cf-image-received/main.py
+++ cf-image-received/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 from os import getenv
 
 import requests
@@ -31,55 +32,67 @@ def ack_image_received(data, context):
         None; the output is written to Stackdriver Logging
     """
 
-    file_path = data['name']
-    file_id = data['id']  # Includes bucket & storage generation
-    print(f"Received: {file_id} at {file_path}")
+    bucket_path = data['name']
+    object_id = data['id']  # Includes bucket & storage generation
+    print(f"Received: {object_id} at {bucket_path}")
 
-    _, file_ext = os.path.splitext(file_path)
+    _, file_ext = os.path.splitext(bucket_path)
 
     process_lookup = {
         '.fz': process_fits,
         '.cr2': process_cr2,
     }
 
-    print(f"Processing {file_id}")
-    process_lookup[file_ext](file_path, file_id)
+    print(f"Processing {object_id}")
+    process_lookup[file_ext](bucket_path, object_id)
 
 
-def process_fits(file_path, file_id):
+def process_fits(bucket_path, object_id):
     """ Thin-wrapper around process_file that sets PIAA_STATE """
     headers = {
-        'PIAA_STATE': 'fits_received'
+        'PSTATE': 'fits_received'
     }
-    process_file(file_path, file_id, headers)
+    process_file(bucket_path, object_id, headers)
 
 
-def process_cr2(file_path, file_id):
+def process_cr2(bucket_path, object_id):
     pass
 
 
-def process_file(file_path, file_id, headers):
+def process_file(bucket_path, object_id, headers):
     """ Forward the headers to the -add-header-to-db Cloud Function.
 
     Args:
-        file_path (str): The relative (to the bucket) path of the file in the storage bucket.
-        file_id (str): The full blob id for the file in the storage bucket, includes generation.
+        bucket_path (str): The relative (to the bucket) path of the file in the storage bucket.
+        object_id (str): The full blob id for the file in the storage bucket, includes generation.
         headers (dict): A dictionary of header values to be stored with header values from image.
     """
     # Get some of the fields from the path.
-    unit_id, field, camera_id, seq_time, filename = file_path.split('/')
+    unit_id, field, camera_id, seq_time, filename = bucket_path.split('/')
+
+    # Get the image time from the filename
+    image_time = filename.split('.')[0]
+
+    # Build the sequence and image ids
+    sequence_id = f'{unit_id}_{camera_id}_{seq_time}'
+    image_id = f'{unit_id}_{camera_id}_{image_time}'
 
     headers.update({
         'PANID': unit_id,
         'FIELD': field,
-        'FILENAME': file_path,
-        'FILEID': file_id,
+        'INSTRUME': camera_id,
+        'SEQTIME': seq_time,
+        'IMGTIME': image_time,
+        'SEQID': sequence_id,
+        'IMAGEID': image_id,
+        'FILENAME': bucket_path,
+        'FILEID': object_id,
     })
 
     # Send to add-header-to-db
     print(f"Forwarding to add-header-to-db: {headers!r}")
     requests.post(add_header_endpoint, json={
         'headers': headers,
-        'bucket_path': file_path,
-        'object_id': file_id,
+        'bucket_path': bucket_path,
+        'object_id': object_id,
     })
