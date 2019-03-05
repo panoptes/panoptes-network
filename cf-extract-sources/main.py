f6449206ed2f5696674bb031aec3ba0ef204560e
Wilfred Gee <wtylergee@gmail.com>
2019-03-05 17:24:08 +1100

    CF for extracting sources
    * Adding the utils functions from `piaa.utils.helpers` directly to CF. NOTE: Need a way to mitigate this.

--- cf-extract-sources/main.py
+++ cf-extract-sources/main.py
@@ -2,6 +2,8 @@ import os
 from flask import jsonify
 import pandas as pd
 
+from decimal import Decimal
+
 from psycopg2.extras import execute_values
 from psycopg2 import IntegrityError
 from psycopg2 import OperationalError
@@ -9,10 +11,7 @@ from psycopg2.pool import SimpleConnectionPool
 
 from astropy.io import fits
 
-from google.cloud import logging
 from google.cloud import storage
-
-from piaa.utils import helpers
 
 PROJECT_ID = os.getenv('PROJECT_ID', 'panoptes-survey')
 
@@ -70,7 +69,8 @@ def extract_sources(request):
         return f'No file requested'
 
     print(f"Processing {csv_bucket_path}")
-    local_csv = download_blob(csv_bucket_path, bucket_name='panoptes-detected-sources')
+    local_csv = download_blob(csv_bucket_path, destination='/tmp',
+                              bucket_name='panoptes-detected-sources')
     point_sources = pd.read_csv(local_csv)
 
     global pg_pool
@@ -114,7 +114,7 @@ def get_sources(point_sources, stamp_size=10, cursor=None):
     remove_columns = ['picid', 'image_id', 'ra', 'dec',
                       'x_image', 'y_image', 'seq_time', 'img_time']
 
-    logging.info(f'Getting point sources')
+    print(f'Getting point sources')
 
     for picid, row in point_sources.iterrows():
         image_id = row.image_id
@@ -127,7 +127,7 @@ def get_sources(point_sources, stamp_size=10, cursor=None):
             fits_bucket_name = fits_fn
 
         # Get the stamp for the target
-        target_slice = helpers.get_stamp_slice(
+        target_slice = get_stamp_slice(
             row.x, row.y,
             stamp_size=(stamp_size, stamp_size),
             ignore_superpixel=False,
@@ -168,15 +168,15 @@ def get_sources(point_sources, stamp_size=10, cursor=None):
         insert_sql = f'INSERT INTO sources ({",".join(headers)}) VALUES %s'
         insert_template = '(' + ','.join([f'%({h})s' for h in headers]) + ')'
 
-        logging.info(f'Inserting {len(source_metadata)} metadata for {fits_fn}')
+        print(f'Inserting {len(source_metadata)} metadata for {fits_fn}')
         execute_values(cursor, insert_sql, source_metadata, insert_template)
         cursor.connection.commit()
     except IntegrityError:
-        logging.info(f'Sources information already loaded into database')
+        print(f'Sources information already loaded into database')
     finally:
-        logging.info(f'Copy of metadata complete {fits_fn}')
+        print(f'Copy of metadata complete {fits_fn}')
 
-    # logging.info(f'Done inserting metadata, building dataframe for data.')
+    # print(f'Done inserting metadata, building dataframe for data.')
     # try:
     #     stamp_df = pd.DataFrame(
     #         sources,
@@ -192,13 +192,13 @@ def get_sources(point_sources, stamp_size=10, cursor=None):
     #             'pixel_value'
     #         ]).set_index(['picid', 'image_time'])
     # except AssertionError as e:
-    #     logging.info(f'Error writing dataframe to BigQuery: {e!r}')
+    #     print(f'Error writing dataframe to BigQuery: {e!r}')
     # else:
-    #     logging.info(f'Done building dataframe, sending to BigQuery')
+    #     print(f'Done building dataframe, sending to BigQuery')
     #     job = bq_client.load_table_from_dataframe(stamp_df, bq_sources_table)
     #     job.result()  # Waits for table load to complete.
     #     if job.state != 'DONE':
-    #         logging.info(f'Failed to send dataframe to BigQuery')
+    #         print(f'Failed to send dataframe to BigQuery')
 
     return image_id
 
@@ -238,9 +238,9 @@ def update_state(state, sequence_id=None, image_id=None, cursor=None, **kwargs):
             cursor.connection.rollback()
             cursor.execute(update_sql, [state, field])
         except Exception as e:
-            logging.info(f"Error in insert (error): {e!r}")
-            logging.info(f"Error in insert (sql): {update_sql}")
-            logging.info(f"Error in insert (kwargs): {kwargs!r}")
+            print(f"Error in insert (error): {e!r}")
+            print(f"Error in insert (sql): {update_sql}")
+            print(f"Error in insert (kwargs): {kwargs!r}")
             return False
 
     return True
@@ -263,9 +263,166 @@ def download_blob(source_blob_name, destination=None, bucket=None, bucket_name='
 
     blob.download_to_filename(destination)
 
-    logging.info('Blob {} downloaded to {}.'.format(source_blob_name, destination))
+    print('Blob {} downloaded to {}.'.format(source_blob_name, destination))
 
     return destination
+
+
+def get_stamp_slice(x, y, stamp_size=(14, 14), verbose=False, ignore_superpixel=False):
+    """Get the slice around a given position with fixed Bayer pattern.
+
+    Given an x,y pixel position, get the slice object for a stamp of a given size
+    but make sure the first position corresponds to a red-pixel. This means that
+    x,y will not necessarily be at the center of the resulting stamp.
+
+    Args:
+        x (float): X pixel position.
+        y (float): Y pixel position.
+        stamp_size (tuple, optional): The size of the cutout, default (14, 14).
+        verbose (bool, optional): Verbose, default False.
+
+    Returns:
+        `slice`: A slice object for the data.
+    """
+    # Make sure requested size can have superpixels on each side.
+    if not ignore_superpixel:
+        for side_length in stamp_size:
+            side_length -= 2  # Subtract center superpixel
+            if int(side_length / 2) % 2 != 0:
+                print("Invalid slice size: ", side_length + 2,
+                      " Slice must have even number of pixels on each side of",
+                      " the center superpixel.",
+                      "i.e. 6, 10, 14, 18...")
+                return
+
+    # Pixels have nasty 0.5 rounding issues
+    x = Decimal(float(x)).to_integral()
+    y = Decimal(float(y)).to_integral()
+    color = pixel_color(x, y)
+    if verbose:
+        print(x, y, color)
+
+    x_half = int(stamp_size[0] / 2)
+    y_half = int(stamp_size[1] / 2)
+
+    x_min = int(x - x_half)
+    x_max = int(x + x_half)
+
+    y_min = int(y - y_half)
+    y_max = int(y + y_half)
+
+    # Alter the bounds depending on identified center pixel
+    if color == 'B':
+        x_min -= 1
+        x_max -= 1
+        y_min -= 0
+        y_max -= 0
+    elif color == 'G1':
+        x_min -= 1
+        x_max -= 1
+        y_min -= 1
+        y_max -= 1
+    elif color == 'G2':
+        x_min -= 0
+        x_max -= 0
+        y_min -= 0
+        y_max -= 0
+    elif color == 'R':
+        x_min -= 0
+        x_max -= 0
+        y_min -= 1
+        y_max -= 1
+
+    # if stamp_size is odd add extra
+    if (stamp_size[0] % 2 == 1):
+        x_max += 1
+        y_max += 1
+
+    if verbose:
+        print(x_min, x_max, y_min, y_max)
+        print()
+
+    return (slice(y_min, y_max), slice(x_min, x_max))
+
+
+def pixel_color(x, y):
+    """ Given an x,y position, return the corresponding color.
+
+    The Bayer array defines a superpixel as a collection of 4 pixels
+    set in a square grid:
+
+                     R G
+                     G B
+
+    `ds9` and other image viewers define the coordinate axis from the
+    lower left corner of the image, which is how a traditional x-y plane
+    is defined and how most images would expect to look when viewed. This
+    means that the `(0, 0)` coordinate position will be in the lower left
+    corner of the image.
+
+    When the data is loaded into a `numpy` array the data is flipped on the
+    vertical axis in order to maintain the same indexing/slicing features.
+    This means the the `(0, 0)` coordinate position is in the upper-left
+    corner of the array when output. When plotting this array one can use
+    the `origin='lower'` option to view the array as would be expected in
+    a normal image although this does not change the actual index.
+
+    Note:
+
+        Image dimensions:
+
+         ----------------------------
+         x | width  | i | columns |  5208
+         y | height | j | rows    |  3476
+
+        Bayer Pattern:
+
+                                      x / j
+
+                      0     1    2     3 ... 5204 5205 5206 5207
+                    --------------------------------------------
+               3475 |  R   G1    R    G1        R   G1    R   G1
+               3474 | G2    B   G2     B       G2    B   G2    B
+               3473 |  R   G1    R    G1        R   G1    R   G1
+               3472 | G2    B   G2     B       G2    B   G2    B
+                  . |
+         y / i    . |
+                  . |
+                  3 |  R   G1    R    G1        R   G1    R   G1
+                  2 | G2    B   G2     B       G2    B   G2    B
+                  1 |  R   G1    R    G1        R   G1    R   G1
+                  0 | G2    B   G2     B       G2    B   G2    B
+
+
+        This can be described by:
+
+                 | row (y) |  col (x)
+             --------------| ------
+              R  |  odd i, |  even j
+              G1 |  odd i, |   odd j
+              G2 | even i, |  even j
+              B  | even i, |   odd j
+
+            bayer[1::2, 0::2, 0] = 1 # Red
+            bayer[1::2, 1::2, 1] = 1 # Green
+            bayer[0::2, 0::2, 1] = 1 # Green
+            bayer[0::2, 1::2, 2] = 1 # Blue
+
+    Returns:
+        str: one of 'R', 'G1', 'G2', 'B'
+    """
+    x = int(x)
+    y = int(y)
+    if x % 2 == 0:
+        if y % 2 == 0:
+            return 'G2'
+        else:
+            return 'R'
+    else:
+        if y % 2 == 0:
+            return 'B'
+        else:
+            return 'G1'
 
 
 def __connect(host):
