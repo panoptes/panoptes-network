1dec74a5a69b67ace7e1edf2b404b15035a2ac62
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-03-09 09:01:46 +1100

    Acknowledge message before solving (throw error if not solved)

--- gce-plate-solver/plate-solver.py
+++ gce-plate-solver/plate-solver.py
@@ -63,6 +63,9 @@ def msg_callback(message):
     bucket_path = attributes['bucket_path']
     object_id = attributes['object_id']
 
+    # Acknowledge message
+    message.ack()
+
     try:
         # Get DB cursors
         catalog_db_cursor = get_cursor(port=5433, db_name='v702', db_user='panoptes')
@@ -72,7 +75,6 @@ def msg_callback(message):
         solve_file(bucket_path, object_id, catalog_db_cursor, metadata_db_cursor)
     finally:
         print(f'Finished processing {object_id}.')
-        message.ack()
         catalog_db_cursor.close()
         metadata_db_cursor.close()
 
