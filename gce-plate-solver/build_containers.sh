8466de33ae7559d082d721991256152dfedc4ef1
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-05-18 16:08:29 +1000

    Many updates to plate solver (WIP)
    * Fixing CMD and ENTRYPOINT for plate-solver (WIP)
    * Cloud SQL connection items are in `panoptes-utils` so some cleanup
    here.
    *

--- gce-plate-solver/build_containers.sh
+++ gce-plate-solver/build_containers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
+SOURCE_DIR=${PANDIR}/panoptes-network/gce-plate-solver
 
 echo "Building solve-extract Docker GCE instance"
-
 gcloud builds submit \
-	--timeout=1500 \
-	--tag gcr.io/panoptes-survey/solve-extract
+    --timeout="5h" \
+    --config "${SOURCE_DIR}/cloudbuild.yaml"
+
+echo "Updating GCE container"
+gcloud compute instances update-container solve-and-extract
