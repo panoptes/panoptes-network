6e1b00c6b135bc4fd12db0cb87a90b1bd5d6d003
Wilfred Gee <wtylergee@gmail.com>
2019-05-22 15:21:42 +1000

    * Add gcloudignore file * Cleanup for solve kluster

--- kube-solve-and-extract-cluster/make-node-pool-similar-sources.sh
+++ kube-solve-and-extract-cluster/make-node-pool-similar-sources.sh
@@ -2,15 +2,15 @@
 
 gcloud container node-pools create \
 	similar-source-finder-pool \
-	--cluster solve-and-extract-cluster \
+	--cluster solve-extract-find-cluster \
 	--zone us-central1-a \
 	--enable-autorepair \
 	--enable-autoupgrade \
 	--machine-type n1-standard-16 \
 	--preemptible \
-	--node-labels=for=similar-sources \
+	--node-labels=use=piaa \
 	--num-nodes 2 \
 	--enable-autoscaling  \
-	--max-nodes=3 \
+	--max-nodes=5 \
 	--min-nodes=1 \
 	--scopes=storage-rw,logging-write,pubsub
