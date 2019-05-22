6af60ac24c7797c326f537bca47873e30dd19b63
Wilfred Gee <wtylergee@gmail.com>
2019-05-22 16:43:57 +1000

    Updating build script and yaml config for kubernetes clusters

--- kube-solve-and-extract-cluster/make-node-pool-solve-extract-find-sources.sh
+++ kube-solve-and-extract-cluster/make-node-pool-solve-extract-find-sources.sh
@@ -1,16 +1,47 @@
 #!/bin/bash -e
 
-gcloud container node-pools create \
-	similar-source-finder-pool \
-	--cluster solve-extract-find-cluster \
-	--zone us-central1-a \
-	--enable-autorepair \
-	--enable-autoupgrade \
-	--machine-type n1-standard-16 \
+gcloud beta container \
+	--project "panoptes-survey" clusters create "solve-extract-find-cluster" \
+	--zone "us-central1-a" \
+	--no-enable-basic-auth \
+	--cluster-version "1.12.7-gke.10" \
+	--machine-type "n1-standard-1" \
+	--image-type "COS_CONTAINERD" \
+	--disk-type "pd-standard" \
+	--disk-size "100" \
+	--node-labels stage=solve-extract,use=piaa \
+	--metadata disable-legacy-endpoints=true,google-logging-enabled=true \
+	--scopes "https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/sqlservice.admin","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
 	--preemptible \
-	--node-labels=use=piaa \
-	--num-nodes 2 \
-	--enable-autoscaling  \
-	--max-nodes=5 \
-	--min-nodes=1 \
-	--scopes=storage-rw,logging-write,pubsub
+	--num-nodes "3" \
+	--enable-stackdriver-kubernetes \
+	--no-enable-ip-alias \
+	--network "projects/panoptes-survey/global/networks/default" \
+	--enable-autoscaling \
+	--min-nodes "1" \
+	--max-nodes "5" \
+	--addons HorizontalPodAutoscaling,HttpLoadBalancing \
+	--enable-autoupgrade \
+	--enable-autorepair \
+	--labels use=piaa \
+	--enable-vertical-pod-autoscaling
+
+gcloud beta container \
+	--project "panoptes-survey" node-pools create "find-similar-sources-pool" \
+	--cluster "solve-extract-find-cluster" \
+	--zone "us-central1-a" \
+	--node-version "1.12.7-gke.10" \
+	--machine-type "n1-standard-2" \
+	--image-type "COS_CONTAINERD" \
+	--disk-type "pd-standard" \
+	--disk-size "100" \
+	--node-labels stage=find-similar-sources,use=piaa \
+	--metadata disable-legacy-endpoints=true,google-logging-enabled=true \
+	--scopes "https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/sqlservice.admin","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+	--preemptible \
+	--num-nodes "3" \
+	--enable-autoscaling \
+	--min-nodes "1" \
+	--max-nodes "5" \
+	--enable-autoupgrade \
+	--enable-autorepair
