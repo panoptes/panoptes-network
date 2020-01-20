5290121153fcf2eddf3a5c58b30578e46a3f8023
Wilfred Gee <wtylergee@gmail.com>
2020-01-20 09:15:00 -1000

    Removing cf for get-state, which can be replaced by firestore

--- cf-get-state/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "Deploying cloud function: cf-get-state"
-
-gcloud functions deploy \
-                 get-state \
-                 --entry-point get_state \
-                 --runtime python37 \
-                 --trigger-http
