e51430918bb60514355c7445e3b4272a9f833fb6
Wilfred Gee <wtylergee@gmail.com>
2020-01-27 11:32:18 -1000

    Format

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-const admin = require('firebase-admin');
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -13,80 +13,83 @@ const db = admin.firestore();
 const increment = admin.firestore.FieldValue.increment(1);
 const decrement = admin.firestore.FieldValue.increment(-1);
 
+export const getRecentObservations = functions.https.onCall((data, context) => {
+
+});
 
 export const getUnits = functions.https.onCall((data, context) => {
-  const units:any[] = [];
+  const units: any[] = [];
   return db.collection("units").get()
-  .then((querySnapshot:FirebaseFirestore.QuerySnapshot) => {
-    querySnapshot.forEach((doc:FirebaseFirestore.QueryDocumentSnapshot) => {
+    .then((querySnapshot: FirebaseFirestore.QuerySnapshot) => {
+      querySnapshot.forEach((doc: FirebaseFirestore.QueryDocumentSnapshot) => {
         const unitData = doc.data();
         unitData['unit_id'] = doc.id;
         units.push(unitData);
+      });
+      return units;
+    })
+    .catch((err: any) => {
+      console.error(err);
     });
-    return units;
-  })
-  .catch((err:any) => {
-    console.error(err);
-  });
- });
+});
 
 export const imagesCountIncrement = functions.firestore
-    .document('images/{imageId}')
-    .onCreate((snap, context) => {
-      // Get the unit_id from the image id.
-      const unitId = context.params.imageId.split('_')[0];
-      const observationId:string = snap.get('sequence_id');
+  .document('images/{imageId}')
+  .onCreate((snap, context) => {
+    // Get the unit_id from the image id.
+    const unitId = context.params.imageId.split('_')[0];
+    const observationId: string = snap.get('sequence_id');
 
-      const unitRef = db.collection('units').doc(unitId);
-      const obsRef = db.collection('observations').doc(observationId);
-  
-      db.runTransaction((transaction:any) => {
-        transaction.get(unitRef).then(() => {
-          transaction.update(unitRef, {
-            num_images: increment,
-          });
-          transaction.update(obsRef, {
-            num_images: increment,
-          });          
+    const unitRef = db.collection('units').doc(unitId);
+    const obsRef = db.collection('observations').doc(observationId);
+
+    return db.runTransaction((transaction) => {
+      transaction.get(unitRef).then(() => {
+        transaction.update(unitRef, {
+          num_images: increment,
         });
-      })
-      .then(() => console.log('Image count incremented') )
-      .catch((err:any) => { console.log(err) });
-    });
+        transaction.update(obsRef, {
+          num_images: increment,
+        });
+      });
+    })
+      .then(() => console.log('Image count incremented'))
+      .catch((err: any) => { console.log(err) });
+  });
 
 export const imagesCountDecrement = functions.firestore
-    .document('images/{imageId}')
-    .onCreate((snap, context) => {
-      // Get the unit_id from the image id.
-      const unitId = context.params.imageId.split('_')[0];
-      const observationId:string = snap.get('sequence_id');
+  .document('images/{imageId}')
+  .onCreate((snap, context) => {
+    // Get the unit_id from the image id.
+    const unitId = context.params.imageId.split('_')[0];
+    const observationId: string = snap.get('sequence_id');
 
-      const unitRef = db.collection('units').doc(unitId);
-      const obsRef = db.collection('observations').doc(observationId);
-  
-      db.runTransaction((transaction:any) => {
-        transaction.get(unitRef).then(() => {
-          transaction.update(unitRef, {
-            num_images: decrement,
-          });
-          transaction.update(obsRef, {
-            num_images: decrement,
-          });          
+    const unitRef = db.collection('units').doc(unitId);
+    const obsRef = db.collection('observations').doc(observationId);
+
+    db.runTransaction((transaction: any) => {
+      transaction.get(unitRef).then(() => {
+        transaction.update(unitRef, {
+          num_images: decrement,
         });
-      })
-      .then(() => console.log('Image count decremented') )
-      .catch((err:any) => { console.log(err) });
-    });    
+        transaction.update(obsRef, {
+          num_images: decrement,
+        });
+      });
+    })
+      .then(() => console.log('Image count decremented'))
+      .catch((err: any) => { console.log(err) });
+  });
 
 export const obsevationsCountIncrement = functions.firestore
   .document('observations/{observationId}')
   .onCreate((snap, context) => {
     // Get a reference to the unit
-    const unitId:string = snap.get('unit_id');
+    const unitId: string = snap.get('unit_id');
     const unitRef = db.collection('units').doc(unitId);
 
     // Update aggregations in a transaction
-    db.runTransaction((transaction:any) => {
+    db.runTransaction((transaction: any) => {
       transaction.get(unitRef).then(() => {
         // increment count
         transaction.update(unitRef, {
@@ -94,26 +97,26 @@ export const obsevationsCountIncrement = functions.firestore
         });
       });
     })
-    .then(() => console.log('Observation count incremented') )
-    .catch((err:any) => { console.log(err) });
+      .then(() => console.log('Observation count incremented'))
+      .catch((err: any) => { console.log(err) });
   });
 
 export const obsevationsCountDecrement = functions.firestore
-    .document('observations/{observationId}')
-    .onDelete((snap, context) => {
-      // Get a reference to the unit
-      const unitId:string = snap.get('unit_id');
-      const unitRef = db.collection('units').doc(unitId);
+  .document('observations/{observationId}')
+  .onDelete((snap, context) => {
+    // Get a reference to the unit
+    const unitId: string = snap.get('unit_id');
+    const unitRef = db.collection('units').doc(unitId);
 
-      // Update aggregations in a transaction
-      db.runTransaction((transaction:any) => {
-        transaction.get(unitRef).then(() => {
-          // decrement count
-          transaction.update(unitRef, {
-            num_observations: decrement,
-          });
+    // Update aggregations in a transaction
+    db.runTransaction((transaction: any) => {
+      transaction.get(unitRef).then(() => {
+        // decrement count
+        transaction.update(unitRef, {
+          num_observations: decrement,
         });
-      })
-      .then(() => console.log('Observation count decremented') )
-      .catch((err:any) => { console.log(err) });
-    });
+      });
+    })
+      .then(() => console.log('Observation count decremented'))
+      .catch((err: any) => { console.log(err) });
+  });
