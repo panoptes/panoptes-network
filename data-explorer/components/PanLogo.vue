3f60e5a4dae8ab6b7a31cba956a7b3c7b0a8d435
Wilfred Gee <wtylergee@gmail.com>
2020-01-29 20:59:12 -1000

    Get rid of terrible spin on logo

--- data-explorer/components/PanLogo.vue
+++ data-explorer/components/PanLogo.vue
@@ -1,16 +1,18 @@
 <template>
-  <img
-    class="PanLogo"
-    alt="PANOPTES Logo"
-    src="https://projectpanoptes.org/uploads/2018/12/17/pan-head.png"
-  />
+  <v-layout column justify-center align-center>
+    <img
+      class="PanLogo"
+      alt="PANOPTES Logo"
+      src="https://projectpanoptes.org/uploads/2018/12/17/pan-head.png"
+    />
+  </v-layout>
 </template>
 
 <style>
 .PanLogo {
   width: 180px;
-  transform: rotateY(560deg);
-  animation: turn 3.5s ease-out forwards 1s;
+  /* transform: rotateY(560deg); */
+  /* animation: turn 3.5s ease-out forwards 1s; */
 }
 
 @keyframes turn {
