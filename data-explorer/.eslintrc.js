4f6dc7007ed370c011b9bc43c4dd1c0be4903ec3
Wilfred Gee <wtylergee@gmail.com>
2020-02-01 15:14:25 -0700

    Don't give prettier errors for now

--- data-explorer/.eslintrc.js
+++ data-explorer/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    // 'plugin:prettier/recommended'
   ],
   // required to lint *.vue files
   plugins: [
@@ -26,6 +26,6 @@ module.exports = {
     'semi': [2, 'never'],
     'no-console': 'off',
     'vue/max-attributes-per-line': 'off',
-    'prettier/prettier': ['error', { 'semi': false }]
+    // 'prettier/prettier': ['error', { 'semi': false }]
   }
 }
