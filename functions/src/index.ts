d64a57bece1dc3e716290eec37b181bd54c05f36
Wilfred Gee <wtylergee@gmail.com>
2020-04-06 12:36:33 -1000

    Clear out functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
-import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
+// import * as functions from 'firebase-functions'
+// import * as admin from 'firebase-admin'
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault()
-})
+// admin.initializeApp({
+  // credential: admin.credential.applicationDefault()
+// })
 
