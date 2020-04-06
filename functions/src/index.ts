d2c20f53b87abc91d4947ff3add032005b29fb5c
Wilfred Gee <wtylergee@gmail.com>
2020-04-06 12:48:06 -1000

    Removing all firebse

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-// import * as functions from 'firebase-functions'
-// import * as admin from 'firebase-admin'
-
-// admin.initializeApp({
-  // credential: admin.credential.applicationDefault()
-// })
-
