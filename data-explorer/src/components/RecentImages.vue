7005c777c9f372dbb863d0f77fbfe46d2929c159
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-10-12 18:08:17 -1000

    * Removing legacy data explorer.

--- data-explorer/src/components/RecentImages.vue
+++ data-explorer/src/components/RecentImages.vue
@@ -4,23 +4,32 @@
       <header class="card-header">
         <p class="card-header-title">Recent images</p>
       </header>
-      <div class="card-content">
-        <b-table :data="images" striped hoverable narrowed>
-          <b-table-column field="time" label="Date" v-slot="props">{{ props.row.time.toDate().toLocaleString() }}</b-table-column>
-          <b-table-column field="sequence_id" label="Observation" v-slot="props">{{
-              props.row.sequence_id
-            }}
+      <div class="card-content has-text-left">
+        <b-table :data="images"
+                 striped
+                 hoverable
+                 narrowed
+                 paginated
+                 per-page="5"
+                 default-sort="time"
+                 sort-icon="arrow-up">
+          <b-table-column field="time" label="Date" sortable v-slot="props">
+            {{ props.row.time.toDate().toLocaleString() }}
           </b-table-column>
-          <b-table-column field="id" label="ID" v-slot="props">{{ props.row.id }}</b-table-column>
-          <b-table-column field="coords" label="Coords" v-slot="props">{{ props.row.ra_image.toFixed(2) }}°
+          <b-table-column field="id" label="ID" sortable v-slot="props">{{ props.row.id }}</b-table-column>
+          <b-table-column field="coords" label="Coords" sortable v-slot="props">
+            {{ props.row.ra_image.toFixed(2) }}°
             {{ props.row.dec_image.toFixed(2) }}°
           </b-table-column>
-          <b-table-column field="exptime" label="Exptime" v-slot="props">{{ props.row.exptime }}</b-table-column>
+          <b-table-column field="exptime" label="Exptime" sortable v-slot="props">{{
+              props.row.exptime
+            }}
+          </b-table-column>
           <b-table-column field="public_url" label="URL" v-slot="props">
             <b-button tag="a" :href="props.row.public_url" type="is-link is-light is-small">.FITS</b-button>
           </b-table-column>
-          <b-table-column field="status" label="Status" v-slot="props">
-            <span class="tag is-success is-uppercase">{{ props.row.status }}</span>
+          <b-table-column field="status" label="Status" sortable v-slot="props">
+            <b-tag type="is-success is-uppercase">{{ props.row.status }}</b-tag>
           </b-table-column>
         </b-table>
       </div>
@@ -39,7 +48,7 @@ export default {
     }
   },
   firestore: {
-    images: db.collection('images').orderBy('time', 'desc').limit(10)
+    images: db.collection('images').orderBy('time', 'desc').limit(25)
   },
   methods: {}
 }
--- legacy-data-explorer/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-TOPIC=${1:-data-explorer}
-BASE_TAG=${1:-develop}
-
-gcloud builds submit --substitutions "_TOPIC=${TOPIC},_BASE_TAG=${BASE_TAG}" .
